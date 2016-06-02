806cb718e8f904fb88ddc34bf0b76b41a24798b8
Richard Fedoriuk <richard.fedoriuk@quickplay.com>
2016-06-02 10:38:28 -0400

    removed 'sudo' from azure extensions script as it requires a tty, and is already being run as root (supposedly)

--- devops-user-nopasswd.sh
+++ devops-user-nopasswd.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 echo updating /etc/sudoers.d/waagent
-echo 'devops ALL = (ALL) NOPASSWD: ALL' > /tmp/waagent; sudo chown root:root /tmp/waagent; sudo chmod 440 /tmp/waagent; sudo cp /tmp/waagent /etc/sudoers.d
+echo 'devops ALL = (ALL) NOPASSWD: ALL' > /tmp/waagent; chown root:root /tmp/waagent; chmod 440 /tmp/waagent; cp /tmp/waagent /etc/sudoers.d
 
